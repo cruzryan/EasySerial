717d0e8df248e8a33e972bb3fe1f6d15862e416f
Ryan Cruz <rncb0963@gmail.com>
2017-11-20 18:48:28 -0600

    Added serialReader()

---
+++ EasySerial.cpp
@@ -0,0 +1,22 @@
+/* 
+
+	Please feel free to contribute to this library 
+	
+*/
+
+
+#include "EasySerial.h"
+#include "Arduino.h"
+
+
+EasySerial::serialBegin(int port){ //Starts a serial from [port]!
+	Serial.begin(port);
+}
+
+void EasySerial::serialReader(){ //Returns incoming bytes from Serial
+	 int incomingByte = 0;
+	 if (Serial.available() > 0) {
+                incomingByte = Serial.read();
+                return incomingByte;
+        }
+}
